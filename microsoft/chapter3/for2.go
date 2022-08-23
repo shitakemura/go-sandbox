5ab7606592e77a278706bb523dee8d22cde52d4b
shitakemura <shitakemura@gmail.com>
2022-08-24 07:14:35 +0900

    microsoft chapter3

---
+++ microsoft/chapter3/for2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var num int64
+	rand.Seed(time.Now().Unix())
+	
+	for num != 5 {
+		num = rand.Int63n(15)
+		fmt.Println(num)
+	}
+}
