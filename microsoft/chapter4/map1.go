fcb7681285b635c3358ec15940f0efbe7aa8be72
shitakemura <shitakemura@gmail.com>
2022-08-24 21:30:48 +0900

    microsoft chapter4

---
+++ microsoft/chapter4/map1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	studentsAge := map[string]int{
+		"john": 32,
+		"bob": 31,
+	}
+	fmt.Println(studentsAge)
+}
---
+++ microsoft/chapter4/map6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	studentsAge := make(map[string]int)
+	studentsAge["john"] = 32
+	studentsAge["bob"] = 31
+	for name, age := range studentsAge {
+		fmt.Printf("%s\t%d\n", name, age)
+	}
+}
