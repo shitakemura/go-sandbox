78f8815b7155d5c1aa9b08a7155c7f36cd184e62
shitakemura <shitakemura@gmail.com>
2022-08-24 20:45:53 +0900

    microsoft chapter6

---
+++ microsoft/chapter4/slice1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+	fmt.Println(months)
+	fmt.Println("Length:", len(months))
+	fmt.Println("Capacity:", cap(months))
+}
---
+++ microsoft/chapter4/slice3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+	quarter2 := months[3:6]
+	quarter2Extended := quarter2[:4]
+	fmt.Println(quarter2, len(quarter2), cap(quarter2))
+	fmt.Println(quarter2Extended, len(quarter2Extended), cap(quarter2Extended))
+}
