5de936f309bec8278dbfd583fafc1e4b39cff48a
shitakemura <shitakemura@gmail.com>
2022-08-28 07:44:09 +0900

    microsoft chapter8

--- microsoft/chapter8/bankapi/main.go
+++ microsoft/chapter8/bankapi/main.go
@@ -21,6 +21,8 @@ func main() {
 	}
 
 	http.HandleFunc("/statement", statement)
+	http.HandleFunc("/deposit", deposit)
+	http.HandleFunc("/withdraw", withdraw)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -29,6 +31,7 @@ func statement(w http.ResponseWriter, req *http.Request) {
 
 	if numberqs == "" {
 		fmt.Fprintf(w, "Account number is missing!")
+		return
 	}
 
 	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
@@ -39,6 +42,62 @@ func statement(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "Account with number %v can't be found!", number)
 		} else {
 			fmt.Fprint(w, account.Statement())
+		}
+	}
+}
+
+func deposit(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Deposit(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprint(w, account.Statement())
+			}
+		}
+	}
+}
+
+func withdraw(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Withdraw(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprint(w, account.Statement())
+			}
 		}
 	}
 }
