506abf8eac1f593fef044ae729d66f91783a7ca2
shitakemura <shitakemura@gmail.com>
2022-08-27 14:22:24 +0900

    microsoft chapter7

---
+++ microsoft/chapter7/channel4.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func send(ch chan<- string, message string) {
+	fmt.Printf("Sending: %#v\n", message)
+	// ch <- message
+	ch <- "Bye!"
+}
+
+func read(ch <-chan string) {
+	fmt.Printf("Receiving: %#v\n", <-ch)
+}
+
+func main() {
+	ch := make(chan string, 1)
+	send(ch, "Hello World!")
+	read(ch)
+}
