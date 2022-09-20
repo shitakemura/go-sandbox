baa08632906b980e1e351c1625348ecfeb147ed7
shitakemura <shitakemura@gmail.com>
2022-09-20 20:58:14 +0900

    a tour of go

---
+++ a-tour-of-go/archive/empty-interface.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func main() {
+	var i interface{}
+	describe(i)
+
+	i = "hello"
+	describe(i)
+}
