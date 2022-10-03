02846ae9402bcbb630a126ba2c2a5f25f9073148
shitakemura <shitakemura@gmail.com>
2022-10-03 16:44:17 +0900

    ch8

--- microsoft/ch8/bankcore/bank.go
+++ microsoft/ch8/bankcore/bank.go
@@ -42,3 +42,25 @@ func (a *Account) Withdraw(amount float64) error {
 func (a *Account) Statement() string {
 	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
 }
+
+func (a *Account) Transfer(amount float64, dest *Account) error {
+	if amount <= 0 {
+		return errors.New("the amount to transfer should be greater than zero")
+	}
+
+	if a.Balance < amount {
+		return errors.New("the amount to transfer should be greater than the account's balance")
+	}
+
+	a.Withdraw(amount)
+	dest.Deposit(amount)
+	return nil
+}
+
+type Bank interface {
+	Statement() string
+}
+
+func Statement(b Bank) string {
+	return b.Statement()
+}
