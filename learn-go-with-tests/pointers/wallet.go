708ffe07730fb1ca2871fb6b7f8f04a12b60d5ba
shitakemura <shitakemura@gmail.com>
2022-10-21 21:05:47 +0900

    update pointers package

--- learn-go-with-tests/pointers/wallet.go
+++ learn-go-with-tests/pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -18,4 +21,15 @@ func (w *Wallet) Balance() Bitcoin {
 
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
