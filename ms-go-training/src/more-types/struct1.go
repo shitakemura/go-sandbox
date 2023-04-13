e30ba3af31b83d1e0d0bf3bbedb9167e8a8d06a8
shitakemura <shitakemura@gmail.com>
2023-04-14 00:04:35 +0900

    struct

---
+++ ms-go-training/src/more-types/struct1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Employee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Address   string
+}
+
+func main() {
+	employee := Employee{LastName: "Doe", FirstName: "John"}
+	fmt.Println(employee)
+	employeeCopy := &employee
+	employeeCopy.FirstName = "David"
+	fmt.Println(employee)
+}
