d1556396a0c1cd58736a8fc3f1eda1a5ef63ad11
shitakemura <shitakemura@gmail.com>
2023-04-22 21:52:29 +0900

    ~7.2

--- go-api-book/myapi/services/articles_service.go
+++ go-api-book/myapi/services/articles_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"database/sql"
+	"errors"
+
+	"github.com/shitakemura/myapi/apperrors"
 	"github.com/shitakemura/myapi/models"
 	"github.com/shitakemura/myapi/repositories"
 )
@@ -8,6 +12,7 @@ import (
 func (s *MyAppService) PostArticleService(article models.Article) (models.Article, error) {
 	newArticle, err := repositories.InsertArticle(s.db, article)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Article{}, err
 	}
 
@@ -17,20 +22,31 @@ func (s *MyAppService) PostArticleService(article models.Article) (models.Articl
 func (s *MyAppService) GetArticleListService(page int) ([]models.Article, error) {
 	articleList, err := repositories.SelectArticleList(s.db, page)
 	if err != nil {
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return nil, err
 	}
 
+	if len(articleList) == 0 {
+		err := apperrors.NAData.Wrap(ErrNoData, "no data")
+		return nil, err
+	}
 	return articleList, nil
 }
 
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
 	article, err := repositories.SelectArticleDetail(s.db, articleID)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = apperrors.NAData.Wrap(err, "no data")
+			return models.Article{}, err
+		}
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return models.Article{}, err
 	}
 
 	commentList, err := repositories.SelectCommentList(s.db, articleID)
 	if err != nil {
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return models.Article{}, err
 	}
 
@@ -42,6 +58,11 @@ func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) 
 func (s *MyAppService) PostNiceService(article models.Article) (models.Article, error) {
 	err := repositories.UpdateNiceNum(s.db, article.ID)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = apperrors.NoTargetData.Wrap(err, "does not exist target article")
+			return models.Article{}, err
+		}
+		err = apperrors.UpdateDataFailed.Wrap(err, "fail to update nice num")
 		return models.Article{}, err
 	}
 
--- go-api-book/myapi/services/comments_service.go
+++ go-api-book/myapi/services/comments_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/shitakemura/myapi/apperrors"
 	"github.com/shitakemura/myapi/models"
 	"github.com/shitakemura/myapi/repositories"
 )
@@ -8,6 +9,7 @@ import (
 func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Comment{}, err
 	}
 
