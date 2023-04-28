a5c2e79d60c83cda9216137de34f847611aa8de4
shitakemura <shitakemura@gmail.com>
2023-04-28 14:16:57 +0900

    refactor with run function

--- go_todo_app/go.mod
+++ go_todo_app/go.mod
@@ -1,3 +1,5 @@
 module example/go_todo_app
 
 go 1.20
+
+require golang.org/x/sync v0.1.0
--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// goroutineでHTTPサーバを起動
+	eg.Go(func() error {
+		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示す
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// channelからの通知(終了通知)を待機
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
