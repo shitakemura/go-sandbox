a671447cd3526da3153882e93b23fc6d31546218
shitakemura <shitakemura@gmail.com>
2023-04-30 06:46:08 +0900

    add test for adding task

--- go_todo_app/handler/response.go
+++ go_todo_app/handler/response.go
@@ -9,12 +9,13 @@ import (
 
 type ErrResponse struct {
 	Message string   `json:"message"`
-	Details []string `json:""details,omitempty`
+	Details []string `json:"details,omitempty"`
 }
 
 func RespondJSON(ctx context.Context, w http.ResponseWriter, body any, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	bodyBytes, err := json.Marshal(body)
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		rsp := ErrResponse{
