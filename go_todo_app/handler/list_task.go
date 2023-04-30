3099bf67a65905eb4384e519f6f0123bc6030be5
shitakemura <shitakemura@gmail.com>
2023-04-30 10:23:04 +0900

    adds list task handler

---
+++ go_todo_app/handler/list_task.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"example/go_todo_app/entity"
+	"example/go_todo_app/store"
+	"net/http"
+)
+
+type ListTask struct {
+	Store *store.TaskStore
+}
+
+type task struct {
+	ID     entity.TaskID     `json:"id"`
+	Title  string            `json:"title"`
+	Status entity.TaskStatus `json:"status"`
+}
+
+func (lt *ListTask) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tasks := lt.Store.All()
+
+	rsp := []task{}
+	for _, t := range tasks {
+		rsp = append(rsp, task{
+			ID:     t.ID,
+			Title:  t.Title,
+			Status: t.Status,
+		})
+	}
+	RespondJSON(ctx, w, rsp, http.StatusOK)
+}
