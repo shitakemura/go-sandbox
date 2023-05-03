8fe67f9349a6dad21364af5d6ea27708f7c3da93
shitakemura <shitakemura@gmail.com>
2023-05-03 14:17:08 +0900

    add dictionary test

--- learn-go-with-test/dictionary/dictionary.go
+++ learn-go-with-test/dictionary/dictionary.go
@@ -1,7 +1,38 @@
 package dictionary
 
 type Dictionary map[string]string
+type DictionaryErr string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+var (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// do not need to write *Dictionary, because map is reference type
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
