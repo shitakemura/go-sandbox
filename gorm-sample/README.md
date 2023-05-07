ae285ad6cd5664e611eaf4bd51a818c789bd199b
shitakemura <shitakemura@gmail.com>
2023-05-07 14:00:19 +0900

    create project

---
+++ gorm-sample/README.md
@@ -0,0 +1,4 @@
+## Building a REST API with Golang using Gin and Gorm
+
+- https://blog.logrocket.com/rest-api-golang-gin-gorm/
+- https://gorm.io/ja_JP/docs/index.html
---
+++ gorm-sample/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"gorm-sample/controllers"
+	"gorm-sample/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.Run("localhost:8080")
+}
